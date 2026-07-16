e453ecadcef4b33af74115d59778dc5342d5aadd
Samuel Dauda <smartsam9981@gmail.com>
2026-07-16 08:40:43 +0100

    Stage 3:POST task, Stage 4: PUT tasks/:id

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+const tasks = [
+    {
+        id: 1, 
+        title: "Learn Express", 
+        done: true,
+    },
+    {
+        id: 2,
+        title: "Build Task API",
+        done: false,
+    
+    },
+    {
+        id: 3,
+        title: "Deploy",
+        done: false,
+    }
+]
+
 app.get('/', (req, res) => {
     res.json({ 
         name: "Task API",
@@ -16,6 +35,60 @@ app.get('/health', (req, res)=>{
     res.json({
         status: "OK",
     })
+})
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+    if (task) {
+        res.json(task);
+    } else {
+        res.status(404).json({ error: "Task not found" });
+    }
+});
+
+
+app.post('/tasks', (req, res)=>{
+    const { title } = req.body;
+    if(!title){
+        return res.status(400).json({
+            error: "Title is required"
+        })
+    };
+
+    const newTask = {
+        id: tasks.length + 1,
+        title,
+        done: false,
+    };
+    tasks.push(newTask);
+
+    return res.status(201).json(newTask)
+})
+
+
+app.put('/tasks/:id', (req, res)=>{
+    const id = Number(req.params.id);
+    const { title, done } = req.body;
+    const task = tasks.find((task)=> task.id === id);
+    if(!task){
+        return res.status(404).json({
+            error: "Task not found",
+        })
+    }
+    if(!title || typeof done !== "boolean"){
+        return res.status(400).json({
+            error: "Title and done are required"
+        })
+    }
+
+    task.title = title;
+    task.done = done;
+    res.json(task);
 })
 
 app.listen(PORT, () => {
